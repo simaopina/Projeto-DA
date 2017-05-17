3b591b9f2fadd6f7d58e9a0545bed0ccda4fa08e
simao <simao@SIMOPINA>
2017-05-17 16:43:43 +0100

    Close();

--- Base/WindowsFormsApp1/WindowsFormsApp1/Hitorico_Arbitro.cs
+++ Base/WindowsFormsApp1/WindowsFormsApp1/Hitorico_Arbitro.cs
@@ -21,42 +21,49 @@ namespace WindowsFormsApp1
         {
             Home_Arbitro HArfrm = new Home_Arbitro();
             HArfrm.Show();
+            Close();
         }
 
         private void baralhoToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Baralho_Arbitro Barfrm = new Baralho_Arbitro();
-            Barfrm.Show();       
+            Barfrm.Show();
+            Close();
         }
 
         private void cartasToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Cartas_Arbitro CarArbfrm = new Cartas_Arbitro();
             CarArbfrm.Show();
+            Close();
         }
 
         private void jogosADecorrerToolStripMenuItem_Click(object sender, EventArgs e)
         {
             JogoArbitro ArbJogosfrm = new JogoArbitro();
             ArbJogosfrm.Show();
+            Close();
         }
 
         private void planeamentoDeJogosToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Arbitro_JogosOndeTou ArbOndeToufrm = new Arbitro_JogosOndeTou();
             ArbOndeToufrm.Show();
+            Close();
         }
 
         private void historicoToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Hitorico_Arbitro Histfrm = new Hitorico_Arbitro();
             Histfrm.Show();
+            Close();
         }
 
         private void terminarSessãoToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Home Hfrm = new Home();
             Hfrm.Show();
+            Close();
         }
     }
 }
