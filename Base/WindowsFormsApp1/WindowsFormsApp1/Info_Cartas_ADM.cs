fc5568bbf9e0ce79d0c41ff3cb37f81a5957e4e7
Jéssica Grácio <2160475@my.ipleiria.pt>
2017-05-17 18:53:52 +0100

    info_cartas_ADM V2

--- Base/WindowsFormsApp1/WindowsFormsApp1/Info_Cartas_ADM.cs
+++ Base/WindowsFormsApp1/WindowsFormsApp1/Info_Cartas_ADM.cs
@@ -15,6 +15,40 @@ namespace WindowsFormsApp1
         public Info_Cartas_ADM()
         {
             InitializeComponent();
+
+
+        }
+
+        private void lblNome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Info_Cartas_ADM_Load(object sender, EventArgs e)
+        {
+            string nome = lblNome.Text;
+            string facao = lblFacao.Text;
+            string tipo = lblTipo.Text;
+            int lealdade = Convert.ToInt32(lblLealdade.Text);
+            int custo = Convert.ToInt32(lblCusto.Text);
+            string regras = lblRegras.Text;
+            int ataque = Convert.ToInt32(lblAtaque.Text);
+            int defesa = Convert.ToInt32(lblDefesa.Text);
+            int imagem = 0;
+
+
+             Card carta = new Card
+            {
+                Name = nome,
+                Faction = facao,
+                Type = tipo,
+                Loyalty = lealdade,
+                Cost = custo,
+                RuleText = regras,
+                Attack = ataque,
+                Defense = defesa,
+                Image = imagem
+            };
         }
     }
 }
