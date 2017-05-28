77d0ef88303397e78785055be29047088619cc80
simao <simao@SIMOPINA>
2017-05-28 22:59:49 +0100

    asdsad

--- Base/WindowsFormsApp1/WindowsFormsApp1/InserirEquipa.cs
+++ Base/WindowsFormsApp1/WindowsFormsApp1/InserirEquipa.cs
@@ -36,7 +36,7 @@ namespace WindowsFormsApp1
             {
                 equipaSelected = container.TeamSet.Where(equip => equip.Name.Equals(lbxEquipa.SelectedItem.ToString())).First();
                 tbxNome.Text = equipaSelected.Name;
-                picbxAvatar.Image = Image.FromFile(Path.GetDirectoryName(Application.ExecutablePath) + @"\imagens\" + jogadorSelected.Avatar);
+                picbxAvatar.Image = Image.FromFile(Path.GetDirectoryName(Application.ExecutablePath) + @"\imagens\" + equipaSelected.Avatar);
             }
             else
             {
