47b9a4fe2ca537eeae91c698caa105fd8630fef9
simao <simao@SIMOPINA>
2017-05-29 17:32:16 +0100

    sem rank

--- Base/WindowsFormsApp1/WindowsFormsApp1/Home_ADM.Designer.cs
+++ Base/WindowsFormsApp1/WindowsFormsApp1/Home_ADM.Designer.cs
@@ -35,22 +35,6 @@
             this.rbtnArbitro = new System.Windows.Forms.RadioButton();
             this.btnPesquisar = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.gbxRank = new System.Windows.Forms.GroupBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listRank = new System.Windows.Forms.ListView();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.gbxEquipas = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.listEquipas = new System.Windows.Forms.ListView();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.gbxCuriosidades = new System.Windows.Forms.GroupBox();
-            this.list_Curiosidades = new System.Windows.Forms.ListView();
             this.btnCartas = new System.Windows.Forms.Button();
             this.btnBaralhos = new System.Windows.Forms.Button();
             this.btnEditBaralhos = new System.Windows.Forms.Button();
@@ -78,9 +62,6 @@
             this.torneioEquipaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.terminarSessãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.gbxRank.SuspendLayout();
-            this.gbxEquipas.SuspendLayout();
-            this.gbxCuriosidades.SuspendLayout();
             this.gpbxMenu.SuspendLayout();
             this.menuStripHome.SuspendLayout();
             this.SuspendLayout();
@@ -154,160 +135,6 @@
             this.pictureBox1.Size = new System.Drawing.Size(580, 158);
             this.pictureBox1.TabIndex = 31;
             this.pictureBox1.TabStop = false;
-            // 
-            // gbxRank
-            // 
-            this.gbxRank.Controls.Add(this.label7);
-            this.gbxRank.Controls.Add(this.label6);
-            this.gbxRank.Controls.Add(this.label1);
-            this.gbxRank.Controls.Add(this.listRank);
-            this.gbxRank.Controls.Add(this.label5);
-            this.gbxRank.Controls.Add(this.label4);
-            this.gbxRank.Location = new System.Drawing.Point(35, 205);
-            this.gbxRank.Name = "gbxRank";
-            this.gbxRank.Size = new System.Drawing.Size(179, 100);
-            this.gbxRank.TabIndex = 32;
-            this.gbxRank.TabStop = false;
-            this.gbxRank.Text = "Rank Jogadores";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(15, 71);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(16, 13);
-            this.label7.TabIndex = 17;
-            this.label7.Text = "5.";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(15, 58);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(16, 13);
-            this.label6.TabIndex = 16;
-            this.label6.Text = "4.";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 45);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 13);
-            this.label1.TabIndex = 15;
-            this.label1.Text = "3.";
-            // 
-            // listRank
-            // 
-            this.listRank.Location = new System.Drawing.Point(37, 19);
-            this.listRank.Name = "listRank";
-            this.listRank.Size = new System.Drawing.Size(121, 64);
-            this.listRank.TabIndex = 7;
-            this.listRank.UseCompatibleStateImageBehavior = false;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(15, 32);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(16, 13);
-            this.label5.TabIndex = 14;
-            this.label5.Text = "2.";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(15, 19);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(16, 13);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "1.";
-            // 
-            // gbxEquipas
-            // 
-            this.gbxEquipas.Controls.Add(this.label2);
-            this.gbxEquipas.Controls.Add(this.listEquipas);
-            this.gbxEquipas.Controls.Add(this.label3);
-            this.gbxEquipas.Controls.Add(this.label8);
-            this.gbxEquipas.Controls.Add(this.label10);
-            this.gbxEquipas.Controls.Add(this.label9);
-            this.gbxEquipas.Location = new System.Drawing.Point(35, 321);
-            this.gbxEquipas.Name = "gbxEquipas";
-            this.gbxEquipas.Size = new System.Drawing.Size(179, 100);
-            this.gbxEquipas.TabIndex = 29;
-            this.gbxEquipas.TabStop = false;
-            this.gbxEquipas.Text = "Rank Equipas";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 70);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(16, 13);
-            this.label2.TabIndex = 22;
-            this.label2.Text = "5.";
-            // 
-            // listEquipas
-            // 
-            this.listEquipas.Location = new System.Drawing.Point(37, 19);
-            this.listEquipas.Name = "listEquipas";
-            this.listEquipas.Size = new System.Drawing.Size(121, 64);
-            this.listEquipas.TabIndex = 8;
-            this.listEquipas.UseCompatibleStateImageBehavior = false;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 57);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(16, 13);
-            this.label3.TabIndex = 21;
-            this.label3.Text = "4.";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(15, 44);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(16, 13);
-            this.label8.TabIndex = 20;
-            this.label8.Text = "3.";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(15, 18);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(16, 13);
-            this.label10.TabIndex = 18;
-            this.label10.Text = "1.";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(15, 31);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(16, 13);
-            this.label9.TabIndex = 19;
-            this.label9.Text = "2.";
-            // 
-            // gbxCuriosidades
-            // 
-            this.gbxCuriosidades.Controls.Add(this.list_Curiosidades);
-            this.gbxCuriosidades.Location = new System.Drawing.Point(35, 445);
-            this.gbxCuriosidades.Name = "gbxCuriosidades";
-            this.gbxCuriosidades.Size = new System.Drawing.Size(179, 100);
-            this.gbxCuriosidades.TabIndex = 30;
-            this.gbxCuriosidades.TabStop = false;
-            this.gbxCuriosidades.Text = "Curiosidades";
-            // 
-            // list_Curiosidades
-            // 
-            this.list_Curiosidades.Location = new System.Drawing.Point(37, 19);
-            this.list_Curiosidades.Name = "list_Curiosidades";
-            this.list_Curiosidades.Size = new System.Drawing.Size(121, 64);
-            this.list_Curiosidades.TabIndex = 9;
-            this.list_Curiosidades.UseCompatibleStateImageBehavior = false;
             // 
             // btnCartas
             // 
@@ -534,9 +361,6 @@
             this.ClientSize = new System.Drawing.Size(640, 540);
             this.Controls.Add(this.menuStripHome);
             this.Controls.Add(this.label11);
-            this.Controls.Add(this.gbxCuriosidades);
-            this.Controls.Add(this.gbxEquipas);
-            this.Controls.Add(this.gbxRank);
             this.Controls.Add(this.tbxpesquisa);
             this.Controls.Add(this.rbtnCartas);
             this.Controls.Add(this.rbtnTorneio);
@@ -548,11 +372,6 @@
             this.Name = "Home_ADM";
             this.Text = "Home";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.gbxRank.ResumeLayout(false);
-            this.gbxRank.PerformLayout();
-            this.gbxEquipas.ResumeLayout(false);
-            this.gbxEquipas.PerformLayout();
-            this.gbxCuriosidades.ResumeLayout(false);
             this.gpbxMenu.ResumeLayout(false);
             this.menuStripHome.ResumeLayout(false);
             this.menuStripHome.PerformLayout();
@@ -569,22 +388,6 @@
         private System.Windows.Forms.RadioButton rbtnArbitro;
         private System.Windows.Forms.Button btnPesquisar;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.GroupBox gbxRank;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListView listRank;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.GroupBox gbxEquipas;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListView listEquipas;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.GroupBox gbxCuriosidades;
-        private System.Windows.Forms.ListView list_Curiosidades;
         private System.Windows.Forms.Button btnCartas;
         private System.Windows.Forms.Button btnBaralhos;
         private System.Windows.Forms.Button btnEditBaralhos;
