ea53aeac36cbe5709d35a311543cedbb5bfb50ea
Jéssica Grácio <2160475@my.ipleiria.pt>
2017-05-29 22:55:14 +0100

    cartas arbito

--- Base/WindowsFormsApp1/WindowsFormsApp1/Cartas_Arbitro.cs
+++ Base/WindowsFormsApp1/WindowsFormsApp1/Cartas_Arbitro.cs
@@ -13,113 +13,50 @@ namespace WindowsFormsApp1
     public partial class Cartas_Arbitro : Form
     {
         int id_carta = 0;
+
+        string ParteFinalNome;
+
         public Cartas_Arbitro()
         {
             InitializeComponent();
+
+            foreach (Card cartas in container.CardSet)
+            {
+                ListViewItem CartaList = new ListViewItem(cartas.Name);
+                CartaList.SubItems.Add(cartas.Faction);
+                CartaList.SubItems.Add(cartas.Type);
+                CartaList.SubItems.Add(Convert.ToString(cartas.Loyalty));
+                CartaList.SubItems.Add(Convert.ToString(cartas.Cost));
+                CartaList.SubItems.Add(cartas.RuleText);
+                CartaList.SubItems.Add(Convert.ToString(cartas.Attack));
+                CartaList.SubItems.Add(Convert.ToString(cartas.Defense));
+                if (cartas.Image == "SemImagem")
+                {
+                    CartaList.SubItems.Add(cartas.Name);
+                }
+               
+                listVCartas.Items.Add(CartaList);
+            }
         }
 
         public DiagramaEntidadesArcmageContainer container = new DiagramaEntidadesArcmageContainer();
-        private void btAdicionarCartas_Click(object sender, EventArgs e)
-        {
-            string nome = txtNome.Text;
-            string facao = txtFacao.Text;
-            string tipo = txtTipo.Text;
-            int lealdade = Convert.ToInt32(txtLealdade.Text);
-            int custo = Convert.ToInt32(txtLealdade.Text);
-            string regras = txtRegras.Text;
-            int ataque = Convert.ToInt32(NumericAtaque.Text);
-            int defesa = Convert.ToInt32(NumericDefesa.Text);
-            //int imagem = 0;
-
-            Card carta = new Card
-            {
-                Name = nome,
-                Faction = facao,
-                Type = tipo,
-                Loyalty = lealdade,
-                Cost = custo,
-                RuleText = regras,
-                Attack = ataque,
-                Defense = defesa,
-               // Image = imagem
-            };
-
-            container.CardSet.Add(carta);
-            container.SaveChanges();
-
-            refresh_datagrid();
-        }
+       
 
         private void DataGridCartas_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            txtNome.Text = DataGridCartas.CurrentRow.Cells[1].Value.ToString();
-            txtFacao.Text = DataGridCartas.CurrentRow.Cells[2].Value.ToString();
-            txtTipo.Text = DataGridCartas.CurrentRow.Cells[3].Value.ToString();
-            txtLealdade.Text = DataGridCartas.CurrentRow.Cells[4].Value.ToString();
-            NumericDefesa.Text = DataGridCartas.CurrentRow.Cells[5].Value.ToString();
-            txtcusto.Text = DataGridCartas.CurrentRow.Cells[6].Value.ToString();
-            txtRegras.Text = DataGridCartas.CurrentRow.Cells[7].Value.ToString();
-            NumericAtaque.Text = DataGridCartas.CurrentRow.Cells[8].Value.ToString();
-            LinkLabelImagem.Text = DataGridCartas.CurrentRow.Cells[9].Value.ToString();
-
-
-            id_carta = (int)DataGridCartas.CurrentRow.Cells[0].Value;
+           
         }
+
+    
 
         public void refresh_datagrid()
         {
 
-            this.cardSetTableAdapter.Fill(this.baseDadosDataSet_Cards.CardSet);
-            DataGridCartas.DataSource = cardSetBindingSource;
 
         }
 
-        private void btAlterarCarta_Click(object sender, EventArgs e)
-        {
-            string nome = txtNome.Text;
-            string facao = txtFacao.Text;
-            string tipo = txtTipo.Text;
-            int lealdade = Convert.ToInt32(txtLealdade.Text);
-            int custo = Convert.ToInt32(txtLealdade.Text);
-            string regras = txtRegras.Text;
-            int ataque = Convert.ToInt32(NumericAtaque.Text);
-            int defesa = Convert.ToInt32(NumericDefesa.Text);
-           // int imagem = 0;
+       
 
-            Card carta;
-
-            carta = container.CardSet.Find(id_carta);
-            carta.Name = nome;
-            carta.Faction = facao;
-            carta.Type = tipo;
-            carta.Loyalty = lealdade;
-            carta.Cost = custo;
-            carta.RuleText = regras;
-            carta.Attack = ataque;
-            carta.Defense = defesa;
-            //carta.Image = imagem;
-
-            container.Entry(carta).State = System.Data.Entity.EntityState.Modified;
-            container.SaveChanges();
-
-            MessageBox.Show("Alterado com sucesso!");
-
-            refresh_datagrid();
-        }
-
-        private void btnRemover_Click(object sender, EventArgs e)
-        {
-            Card carta = container.CardSet.Find(id_carta);
-
-            container.CardSet.Remove(carta);
-
-            container.SaveChanges();
-
-
-            MessageBox.Show("Eliminado com sucesso!");
-
-            refresh_datagrid();
-        }
 
         private void btnPesquisar_Click(object sender, EventArgs e)
         {
@@ -128,7 +65,7 @@ namespace WindowsFormsApp1
 
                 var query = container.CardSet.Where(carta => carta.Name.Contains(tbxpesquisa.Text));
 
-                DataGridCartas.DataSource = query.ToList();
+               // DataGridCartas.DataSource = query.ToList();
             }
 
             else
